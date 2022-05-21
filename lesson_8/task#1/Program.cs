358f019d021d5461da8701ac9b52dffe55dc29d2
Vladislav.Semenenya <vladsmnnya@gmail.com>
2022-05-21 10:22:04 +0300

    completed tasks for 8th lesson

---
+++ lesson_8/task#1/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System.Text.RegularExpressions;
+
+Write("Введите строку: ");
+string line = ReadLine();
+
+Regex regex = new Regex(@"(\w*-\w*){2}");
+MatchCollection matches = regex.Matches(line);
+
+if(matches.Count > 0)
+{
+    foreach(Match match in matches)
+    {
+        WriteLine($"В слове {match.Value} более одного дефиса");
+    }
+}
+else
+{
+    WriteLine("хихи");
+}
---
+++ lesson_8/task#3/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+using System.Text.RegularExpressions;
+
+Write("Введите текст: ");
+var line = ReadLine();
+
+Regex regex = new Regex(@"[0-9]");
+MatchCollection matches = regex.Matches(line);
+
+if (matches.Count > 0)
+{
+    WriteLine("Текст содержит цифры");
+}
+else
+{
+    WriteLine("Текст не содержит цифры");
+}
