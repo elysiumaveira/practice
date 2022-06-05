aa448bf601368da0f5b35ea2952ce57c801f5ec5
Vladislav.Semenenya <vladsmnnya@gmail.com>
2022-06-05 17:16:11 +0300

    completed tasks for 16th lesson

---
+++ lesson_16/task#1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace task1
+{
+    class MyList<T>
+    {
+        protected int index = 0;
+        protected T[] Values = new T[0];
+        
+        public void Add(T value)
+        {
+            Array.Resize(ref Values, index+1);
+
+            Values[index] = value;
+            index++;
+        }
+        public T this[int index]
+        {
+            get => Values[index];
+        }
+        
+        public int Length
+        {
+            get => Values.Length ; 
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList <int> list = new MyList<int>();
+            list.Add(3213123);
+            list.Add(219389);
+            list.Add(92849);
+            WriteLine($"Элемент с индексом 1: {list[1]}");
+            WriteLine($"Длина: {list.Length}");
+        }
+    }
+}
