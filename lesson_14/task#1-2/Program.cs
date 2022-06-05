19471c7b17fca28e858cb9a579726f11d1addefd
Vladislav.Semenenya <vladsmnnya@gmail.com>
2022-06-05 16:31:37 +0300

    completed tasks for 14th lesson

---
+++ lesson_14/task#1-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+namespace task1_2
+{
+    class Program
+    {
+        public delegate void Functions();
+
+        static void Sum()
+        {
+            WriteLine(4 + 5);
+        }
+        static void Mult()
+        {
+            WriteLine(4 * 5);
+        }
+        static void Div()
+        {
+            WriteLine(4 / 5);
+        }
+
+        static void CallDelegate(Functions functions)
+        {
+            functions();
+        }
+        static void Main(string[] args)
+        {
+            Functions functions = new Functions(Sum);
+            functions += Mult;
+            functions += Div;
+            CallDelegate(functions);
+        }
+    }
+}
---
+++ lesson_14/task#3/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+namespace task3
+{
+    class MyInfo
+    {
+        private string name = "Vladislav";
+        public delegate void MyInfoDel(string message);
+        public event MyInfoDel? Call;
+
+        public string Name
+        {
+            get
+            {
+                return name;    // возвращаем значение свойства
+            }
+            set
+            {
+                Call?.Invoke($"Значение поля name измененно с {name} на {value}");   // 2.Вызов события 
+                name = value;   // устанавливаем новое значение свойства
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyInfo m = new MyInfo();
+            m.Call += DisplayMessage;
+            m.Name = "Vladislav Semenenya";
+            void DisplayMessage(string message) => WriteLine(message);
+        }
+    }
+}
