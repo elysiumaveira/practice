3ad7381b389e0a103e54689447220954f7e78b25
Vladislav.Semenenya <vladsmnnya@gmail.com>
2022-06-05 17:58:08 +0300

    completed tasks for 17th lesson

---
+++ lesson_17/task#1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+using static System.Convert;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            string file = "g.txt";
+            File.WriteAllText(file, "");
+
+            for (int i = 0; i < 10; i++)
+            {
+                File.AppendAllText(file, Convert.ToString(random.Next(100)) + " ");
+            }
+
+            string readFile = File.ReadAllText(file);
+            WriteLine($"Числа в файле: {readFile}");
+            string[] numbers = readFile.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(ToInt16(numbers[i]) % 2 == 0)
+                {
+                    result += 1;
+                }
+            }
+
+            WriteLine($"Количество четных чисел в файле: {result}");
+        }
+    }
+}
