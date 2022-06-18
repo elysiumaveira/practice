fd083cc756eb9e7a9d2298cc28356745289f9dae
Vladislav Semenenya <vladsmnnya@gmail.com>
2022-06-18 11:19:12 +0300

    completed tasks for 20th lesson

---
+++ lesson_21/task#1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static int Repl(int number)
+        {
+            return (number % 10 * 100 + number % 100 / 10 * 10 + number / 100);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Task task1 = new Task(() => Console.WriteLine($"1 способ: {Repl(number)}"));
+            task1.Start();
+            task1.Wait();
+
+            Task task2 = Task.Factory.StartNew(() => Console.WriteLine($"2 способ: {Repl(number)}"));
+            task2.Wait();
+
+            Task task3 = Task.Run(() => Console.WriteLine($"3 способ: {Repl(number)}"));
+            task3.Wait();
+        }
+    }
+}
---
+++ lesson_21/task#4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Math;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Factorial(int x)
+        { 
+            double result = Atan(x);
+            Console.WriteLine($"Значение функции Arctg(x) при x={x} будет равно {Math.Round(result, 3)}");
+        }
+
+        static void Main(string[] args)
+        {
+            int a = -10;
+
+            int b = 1;
+
+            Parallel.For(a, b, Factorial);
+        }
+    }
+}
